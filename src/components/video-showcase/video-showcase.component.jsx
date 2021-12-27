ccf826f4ac56189779ca0a305b2f01849dceb4fc
Chika Nna <chika.ekene@gmail.com>
2021-12-27 18:45:21 -0500

    made changes

--- src/components/video-showcase/video-showcase.component.jsx
+++ src/components/video-showcase/video-showcase.component.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./video-showcase.styles.scss";
 import Video from "./assets/temp-video.mp4";
 import ReactPlayer from "react-player";
-
+const url = "https://vimeo.com/146957840";
+const old_url = "https://vimeo.com/302168664";
 const VideoShowcase = () => (
   <div className="video-showcase-container">
     <ReactPlayer
       className="video"
       width="100%"
       height="100%"
-      url="https://vimeo.com/302168664"
+      url={url}
       controls={false}
       muted={true}
       autoplay={true}
