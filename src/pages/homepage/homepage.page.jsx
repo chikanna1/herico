41359c2dfa39a93b1b7ed39b16bbf4219210d60b
Chika Nna <chika.ekene@gmail.com>
2021-12-27 18:38:08 -0500

    made changes

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -4,6 +4,9 @@ import Hero from "../../components/hero/hero.component";
 import ProductShowcase from "../../components/product-showcase/product-showcase.component";
 import VideoShowcase from "../../components/video-showcase/video-showcase.component";
 import HomepageReviews from "../../components/homepage-reviews/homepage-reviews.component";
+import WhyHerico from "../../components/why-herico/why-herico.component";
+import JewelryShowcase from "../../components/jewelry-showcase/jewelry-showcase.component";
+import Footer from "../../components/footer/footer.component";
 
 const HomePage = () => (
   <div className="homepage-container">
@@ -11,6 +14,9 @@ const HomePage = () => (
     <ProductShowcase />
     <VideoShowcase />
     <HomepageReviews />
+    <WhyHerico />
+    <JewelryShowcase />
+    <Footer />
   </div>
 );
 
